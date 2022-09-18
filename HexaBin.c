33609e95505a8dff4704569073154040a7f4746d
Carlos Tavera (Lord Shadow) <cjohnson2001xd@gmail.com>
2022-09-17 22:32:47 -0500

    Update HexaBin.c

--- HexaBin.c
+++ HexaBin.c
@@ -3,13 +3,13 @@
 int main()
 {
     float num;
+    int *hexa;
 
     printf("Ingresa un numero decimal para convertir en hexadecimal binario: ");
     scanf("%f", &num);
 
-    int *hexa;
-
     hexa = &num;
+    
     printf("El numero %f en hexadecimal binario es: %x\n ", num, *hexa);
 
     return 0;
